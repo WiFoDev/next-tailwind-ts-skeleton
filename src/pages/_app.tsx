7846a97f5b393b771d4a7c62c2be37111c36d05a
WiFoDev <lou81804@gmail.com>
2022-11-14 20:38:22 -0500

    fix import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
+import type {AppProps} from "next/app";
+
 import {Layout} from "@/Layout";
 
 import "@/styles/globals.css";
-import type {AppProps} from "next/app";
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
