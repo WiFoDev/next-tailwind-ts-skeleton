67df39f2aa94ec6f5bf14ad6d21891d8c56840d0
WiFoDev <lou81804@gmail.com>
2022-12-19 19:08:34 -0500

    update layout with main grid

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -7,26 +7,26 @@ type LayoutProps = {
 
 export const Layout = ({children}: LayoutProps) => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Head>
         <title>APP</title>
         <meta content="The next generation app" name="My App" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
-      <header className="sticky top-0 z-20 w-full">
-        <nav className="flex pointer-events-none h-16 mx-auto max-w-screen-standar items-center justify-end gap-2 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
+      <header className="sticky top-0 z-20 w-full bg-background/50 backdrop-blur-sm">
+        <nav className="flex h-16 mx-auto max-w-screen-standar items-center justify-end gap-2 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
           <div className="mr-auto text-xl">My APP</div>
           <ul className="text-lg">Navigation List For The Page</ul>
         </nav>
       </header>
-      <main className="relative flex flex-col items-center justify-center w-full h-full">
+      <main className="flex-1 mx-auto w-full grid max-w-screen-standar pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
         {children}
       </main>
       <footer className="relative pb-[env(safe-area-inset-bottom)]">
-        <div className="mx-auto max-w-screen-standar py-12 flex justify-center pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
-          Here is the footer section
+        <div className="mx-auto max-w-screen-standar py-6 flex justify-center pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
+          Made with ❤️ by WiFo
         </div>
       </footer>
-    </>
+    </div>
   );
 };
